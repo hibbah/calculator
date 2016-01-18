f3c0c7357a4d1dc711f56b080b4562c22e267142
Hibbah <wjy721@naver.com>
2016-01-18 16:20:29 +0900

    main1.c modify

--- main-1.c
+++ main-1.c
@@ -7,7 +7,7 @@ int multi(int, int);
 
 int main(int argc, char* argv[])
 {
-	int number1, number2, result;
+	int number11, number2, result;
 	char operator;
 
 	printf("Please enter a expression: ");
